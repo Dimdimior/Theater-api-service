258073d53beb6c1757e1e022235e5f08d7aedd3f
Dmitry Iorgansky <71707019+Dimdimior@users.noreply.github.com>
2023-08-23 15:52:51 +0300

    Registered admin tool

--- Theater/admin.py
+++ Theater/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    Play,
+    Actor,
+    TheatreHall,
+    Performance,
+    Ticket,
+    Reservation,
+    Genre
+)
 
 # Register your models here.
+
+admin.register(Play)
+admin.register(Actor)
+admin.register(TheatreHall)
+admin.register(Performance)
+admin.register(Ticket)
+admin.register(Reservation)
+admin.register(Genre)
