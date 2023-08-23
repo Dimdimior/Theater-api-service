8016fb157b6266b6fa387dc9f32c66d3b62e3b6a
Dmitry Iorgansky <71707019+Dimdimior@users.noreply.github.com>
2023-08-23 16:07:09 +0300

    Created a theatre/urls.py

--- Theater_service_api/settings.py
+++ Theater_service_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "Theater.apps.TheaterConfig",
+    "theatre.apps.TheaterConfig",
 
 ]
 
