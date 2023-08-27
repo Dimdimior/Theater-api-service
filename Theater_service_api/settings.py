79adab315b83352926dc79e6c46be9337c4ad794
Dmitry Iorgansky <71707019+Dimdimior@users.noreply.github.com>
2023-08-27 23:32:35 +0300

    Created dockerignore and .env.sample files

--- Theater_service_api/settings.py
+++ Theater_service_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r&6!!fq2*63a1q8k8-mza-6cfkvsv1uxuspyj%dyvwa*6v-r+("
+SECRET_KEY = (
+    os.environ.get("SECRET_KEY")
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
